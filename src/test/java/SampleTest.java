b41f3feca2198c4dec1cd3901eab2076fd6f8de6
saumya <saumya.gupta@datamato.com>
2017-12-20 18:22:32 +0530

    updated artifact

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,26 @@
+
+import static org.junit.Assert.*; 
+import org.junit.Test;
+public class SampleTest{
+
+
+@Test
+	public void sampleTestPass()
+	
+	{
+		int a=1;
+		int b=2;
+	assertTrue(a + b==3);
+		
+	}
+
+public void sampleTestFail()
+
+{
+	int a=1;
+	int b=2;
+assertTrue(a + b==4);
+	
+}	
+	
+}
